b95a6277b6ae80adef768be571a604acc0cedfc1
qyb <qiuyingbo@gmail.com>
2022-04-01 09:01:54 +0800

    all in block/function

--- start.ts
+++ start.ts
@@ -7,16 +7,18 @@ import {
     startTests
 } from './test';
 
-console.log('-------------------------------');
-console.log('EECS 183 Project 2 Menu Options');
-console.log('-------------------------------');
-console.log('1) Execute testing functions in test.cpp');
-console.log('2) Execute rps() function to play game');
+(function() {
+    console.log('-------------------------------');
+    console.log('EECS 183 Project 2 Menu Options');
+    console.log('-------------------------------');
+    console.log('1) Execute testing functions in test.cpp');
+    console.log('2) Execute rps() function to play game');
 
-const choice = getChoice();
+    const choice = getChoice();
 
-if (choice === 1) {
-    startTests();
-} else if (choice === 2) {
-    rps();
-}
+    if (choice === 1) {
+        startTests();
+    } else if (choice === 2) {
+        rps();
+    }
+})();
